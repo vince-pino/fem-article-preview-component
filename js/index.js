8236f73e35a8889cc2b20533e078c8f1b6e46ec3
Vince Cedie Pino <vincecediepino@gmail.com>
2024-01-12 10:30:00 +0700

    Finish Project

--- js/index.js
+++ js/index.js
@@ -3,7 +3,8 @@
   const tooltiptextEl = document.querySelector('.tooltiptext');
   const descriptionEl = document.querySelector('.description');
 
-  shareBtn.addEventListener('click', () => {
+
+  function handleClick() {
     if (window.innerWidth > 419) {
       tooltiptextEl.style.visibility = tooltiptextEl.style.visibility === 'visible' ? 'hidden' : 'visible';
     }
@@ -12,7 +13,26 @@
       if (authorEl.classList.contains('active-author')) {
         descriptionEl.style = "margin-bottom: 24px";
         authorEl.classList.remove('active-author');
-        authorEl.innerHTML = '<span class="tooltiptext"></span>';
+        authorEl.innerHTML = `
+                              <img src="images/avatar-michelle.jpg" alt="">
+                              <div class="author-info">
+                                <div class="name">
+                                  Michelle Appleton
+                                </div>
+                                <div class="date">
+                                  28 Jun 2020
+                                </div>
+                              </div>
+                              <button class="share tooltip" onclick='handleClick()'>
+                                <img class="icon" src="images/icon-share.svg" alt="">
+                                <span class="tooltiptext">
+                                  <div>Share</div>
+                                  <img src="images/icon-facebook.svg" alt="">
+                                  <img src="images/icon-twitter.svg" alt="">
+                                  <img src="images/icon-pinterest.svg" alt="">
+                                </span>
+                              </button>
+                            `;
       }
       else {
         descriptionEl.style = "margin-bottom: 57px";
@@ -24,10 +44,10 @@
                                 <img src="../images/icon-twitter.svg" alt="">
                                 <img src="../images/icon-pinterest.svg" alt="">
                             </span>
-                            <button class="share tooltip active-btn">
+                            <button class="share tooltip active-btn" onclick='handleClick()'>
                               <img class="icon" src="../images/icon-share.svg" alt="">
                             </button>
                             `;
       }
     }
-  });
+  }
